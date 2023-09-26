8b66d01fd40998ab1bdf903296f1723390d64d48
mattia0419 <pironemattia6@gmail.com>
2023-09-26 16:09:53 +0200

    print hotels

---
+++ result.php
@@ -0,0 +1,84 @@
+<?php
+
+    $hotels = [
+
+        [
+            'name' => 'Hotel Belvedere',
+            'description' => 'Hotel Belvedere Descrizione',
+            'parking' => true,
+            'vote' => 4,
+            'distance_to_center' => 10.4
+        ],
+        [
+            'name' => 'Hotel Futuro',
+            'description' => 'Hotel Futuro Descrizione',
+            'parking' => true,
+            'vote' => 2,
+            'distance_to_center' => 2
+        ],
+        [
+            'name' => 'Hotel Rivamare',
+            'description' => 'Hotel Rivamare Descrizione',
+            'parking' => false,
+            'vote' => 1,
+            'distance_to_center' => 1
+        ],
+        [
+            'name' => 'Hotel Bellavista',
+            'description' => 'Hotel Bellavista Descrizione',
+            'parking' => false,
+            'vote' => 5,
+            'distance_to_center' => 5.5
+        ],
+        [
+            'name' => 'Hotel Milano',
+            'description' => 'Hotel Milano Descrizione',
+            'parking' => true,
+            'vote' => 2,
+            'distance_to_center' => 50
+        ],
+
+    ];
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>
+    <?php 
+    for($i = 0; $i < 5; ++$i){
+        echo $hotels[$i]["name"];
+    }
+    ?>
+    </h1>
+    <p>
+    <?php 
+    for($i = 0; $i < 5; ++$i){
+        echo $hotels[$i]["description"];
+    }
+    ?>
+    </p>
+    <span>
+    <?php 
+    for($i = 0; $i < 5; ++$i){
+        echo $hotels[$i]["vote"];
+    }
+    ?>
+    </span>
+    <span>
+    <?php 
+    for($i = 0; $i < 5; ++$i){
+        echo $hotels[$i]["distance_to_center"];
+    }
+    ?>
+    </span>
+   
+    
+</body>
+</html>
